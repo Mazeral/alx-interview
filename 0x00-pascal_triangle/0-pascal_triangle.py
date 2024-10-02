ca5a94a72d9c5fcf653f51dacfbac6ac941f02c1
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-02 21:10:54 +0200

    Trying to fix the code.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,32 +3,22 @@
 0-pascal_triangle
 """
 
-import math
-
 
 def pascal_triangle(n):
     """
-    Generate Pascal's Triangle with 'n' rows.
-
-    Parameters:
-        n (int): Number of rows to generate in Pascal's Triangle.
-
-    Returns:
-        list: A list of lists representing Pascal's Triangle.
-              Each inner list contains the numbers of that row.
+    Returns a list of integers
+    representing the Pascal Triangle of n
+    returns empty list if n <= 0
     """
+    k = []
     if n <= 0:
-        return []
-    else:
-        pascal = []
-        for i in range(n):
-            pascal_row = []
-            j = 0
-            while j <= i:
-                if j == 0 or j == i:
-                    pascal_row.append(1)
-                else:
-                    pascal_row.append(pascal[i-1][j-1] + pascal[i-1][j])
-                j += 1
-            pascal.append(pascal_row)
-    return pascal
+        return k
+    k = [[1]]
+    for i in range(1, n):
+        temp = [1]
+        for j in range(len(k[i - 1]) - 1):
+            curr = k[i - 1]
+            temp.append(k[i - 1][j] + k[i - 1][j + 1])
+        temp.append(1)
+        k.append(temp)
+    return k
