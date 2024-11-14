4995d9baf402a9a12483871586d1f778407cc38f
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-14 13:43:28 +0200

    finished the task.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,30 +5,20 @@ const request = require('request');
 const getNames = async () => {
   const uri = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
-  let characters = [];
-
-  // A helper function to wrap 'request' in a Promise
-  const makeRequest = (uri) => {
+  const helper = (uri) => {
     return new Promise((resolve, reject) => {
-      request(uri, { json: true }, (error, response, body) => {
-        if (error) reject(error);
+      request.get(uri, { json: true }, (err, res, body) => {
+        if (err) reject(err);
         else resolve(body);
       });
     });
   };
 
-  try {
-    // Fetch the film data
-    const data = await makeRequest(uri);
-    characters = data.characters;
-
-    // Loop through each character and fetch their name
-    for (const character of characters) {
-      const characterData = await makeRequest(character);
-      console.log(characterData.name);
-    }
-  } catch (error) {
-    console.error('Error:', error);
+  const films = await helper(uri);
+  const characterLinks = films.characters;
+  for (const link of characterLinks) {
+    const character = (await helper(link));
+    console.log(character.name);
   }
 };
 
