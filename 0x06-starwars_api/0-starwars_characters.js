66ee57e74fc80e0c257ca40f1e0cb29a7c1bb615
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-14 10:22:22 +0200

    task submit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
 
 const uri = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
-let characters = [];
-
-const get_names = async () => {
-  const data = await request.get(uri).then(res => { return res.json(); });
-  characters = data.characters;
-  for (const character of characters) {
-    const name = await request.get(character).then(res => { return res.json(); });
-    console.log(name.name);
+request(uri, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        }
+      });
+    });
   }
-};
-
-get_names();
+});
