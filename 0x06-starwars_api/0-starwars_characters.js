c56fa64fe19654a1d1764c55ed75739fa5f6b65b
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-14 11:07:07 +0200

    finished the task.

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,35 @@
 #!/usr/bin/node
 
-const get_names = async () => {
 const request = require('request');
 
-const uri = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+const getNames = async () => {
+  const uri = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
-let characters = [];
-  const data = await request.get(uri).then(res => { return res.json(); });
-  characters = data.characters;
-  for (const character of characters) {
-    const name = await request.get(character).then(res => { return res.json(); });
-    console.log(name.name);
+  let characters = [];
+
+  // A helper function to wrap 'request' in a Promise
+  const makeRequest = (uri) => {
+    return new Promise((resolve, reject) => {
+      request(uri, { json: true }, (error, response, body) => {
+        if (error) reject(error);
+        else resolve(body);
+      });
+    });
+  };
+
+  try {
+    // Fetch the film data
+    const data = await makeRequest(uri);
+    characters = data.characters;
+
+    // Loop through each character and fetch their name
+    for (const character of characters) {
+      const characterData = await makeRequest(character);
+      console.log(characterData.name);
+    }
+  } catch (error) {
+    console.error('Error:', error);
   }
 };
 
-get_names();
+getNames();
