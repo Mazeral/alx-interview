4023dfd61a22c7949f16342356f2319a6983bf83
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-11-14 10:25:11 +0200

    task submit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,41 +1,17 @@
 #!/usr/bin/node
-/**
- * Wrapper function for request object that allows it
- * to work with async and await
- * @param   {String} url - site url
- * @returns {Promise}    - promise object that resolves
- *                         with parsed JSON response
- *                         and rejects with the request error.
- */
-function makeRequest (url) {
-  const request = require('request');
-  return new Promise((resolve, reject) => {
-    request.get(url, (error, response, body) => {
-      if (error) reject(error);
-      else resolve(JSON.parse(body));
-    });
-  });
-}
+const request = require('request');
 
-/**
- * Entry point - makes requests to Star Wars API
- * for movie info based movie ID passed as a CLI parameter.
- * Retrieves movie character info then prints their names
- * in order of appearance in the initial response.
- */
-async function main () {
-  const args = process.argv;
+const uri = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
 
-  if (args.length < 3) return;
+let characters = [];
 
-  const movieUrl = 'https://swapi-api.alx-tools.com/api/films/' + args[2];
-  const movie = await makeRequest(movieUrl);
-
-  if (movie.characters === undefined) return;
-  for (const characterUrl of movie.characters) {
-    const character = await makeRequest(characterUrl);
-    console.log(character.name);
+const get_names = async () => {
+  const data = await request.get(uri).then(res => { return res.json(); });
+  characters = data.characters;
+  for (const character of characters) {
+    const name = await request.get(character).then(res => { return res.json(); });
+    console.log(name.name);
   }
-}
+};
 
-main();
+get_names();
